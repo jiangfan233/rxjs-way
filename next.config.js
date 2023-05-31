d58b836c84f78d4f801b958b941152d7c9ae9e55
jiangfan233 <lostwind233@outlook.com>
2023-06-01 00:41:54 +0800

    try github cicd

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
+
+// const repo = "https://jiangfan233.github.io/rxjs-way";
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  // 去掉 `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+
 const nextConfig = {
   distDir: "docs",
   reactStrictMode: false,
