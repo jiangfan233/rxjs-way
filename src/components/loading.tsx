c4316c330fc0ab54d412f68adebc3e6a8ddfa311
jiang <jiang@localhost.localdomain>
2023-08-09 22:28:19 +0800

    update

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -4,7 +4,7 @@ import { AiOutlineLoading } from "react-icons/ai"
 
 export const Loading = React.memo(({ size = "4rem" }: { size: string }) => {
   return <div className="flex items-center justify-center w-full h-full">
-    <AiOutlineLoading size={size} />
+    <AiOutlineLoading size={size} className=" animate-spin" />
   </div>
 })
 
