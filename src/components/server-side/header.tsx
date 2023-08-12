121d05d39167999f086af465faa018f69f34ac78
jiang <jiang@localhost.localdomain>
2023-08-12 21:10:29 +0800

    fix

--- src/components/server-side/header.tsx
+++ src/components/server-side/header.tsx
@@ -7,6 +7,7 @@ interface HeaderProps {}
 export const Header: React.FC<HeaderProps> = ({}) => {
   return (
     <div className='global-header mx-auto p-2 w-[calc(100vw-1rem)] sm:w-[calc(60vw-1rem)] h-12 grow flex gap-1 xs:gap-4 items-center justify-between rounded-md fixed top-0 left-1/2 -translate-x-1/2'>
+
       <div className='relative'>
         <a href='#' className='toggle peer relative focus:text-sky-500'>
           <AiOutlineBars className=' cursor-pointer z-10 h-8 text-inherit' />
@@ -14,7 +15,7 @@ export const Header: React.FC<HeaderProps> = ({}) => {
         <Sider />
       </div>
 
-      <h2 className='font-bold text-xl xs:text-2xl grow text-center ml-12'>
+      <h2 className='font-bold text-xl xs:text-2xl grow text-center'>
         Programming Journal
       </h2>
 
