50631b853a99c8b77b4556f3b0657cb930ffe508
jiang <jiang@W11-20230221719.lan>
2023-09-03 14:31:47 +0800

    update

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,12 +1,17 @@
 import { getFileContent } from "@lib/post";
+import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const res = await getFileContent(params.id);
 
+  if(res === null) {
+    notFound();
+  }
+
   return (
     <div
       key={params.id}
-      className='markdown-body w-[calc(100vw-1rem)] sm:w-[calc(60vw-1rem)]'
+      className='markdown-body w-[calc(100vw-1rem)] sm:w-[calc(60vw-1rem)] transition-all duration-300 ease-in'
       dangerouslySetInnerHTML={{
         __html: res ? (res.content as string) : "",
       }}></div>
