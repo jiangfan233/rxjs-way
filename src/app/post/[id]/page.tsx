88e98da9abf2208c6f2d64cf3b1d76c572d47c48
jiang <jiang@W11-20230221719.lan>
2023-09-03 13:52:30 +0800

    fix

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -5,6 +5,7 @@ export default async function Page({ params }: { params: { id: string } }) {
 
   return (
     <div
+      key={params.id}
       className='markdown-body w-[calc(100vw-1rem)] sm:w-[calc(60vw-1rem)]'
       dangerouslySetInnerHTML={{
         __html: res ? (res.content as string) : "",
